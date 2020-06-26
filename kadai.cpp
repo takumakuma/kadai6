7f7255b62f161f303dd14f2fd4cdbb98a4db9795
Takumakuma <q999369@outlook.jp>
2020-06-26 16:38:51 +0900

    create sentence part

--- kadai.cpp
+++ kadai.cpp
@@ -2,17 +2,14 @@
 
 int main(){
 
-    double num1 , num2;
+   char string1[256];
+   char string2[256];
 
-    //2数の入力
-    std::cout << "Import number_1" << std::endl;
-    std::cin >> num1;
-    std::cout << "Import number_2" << std::endl;
-    std::cin >> num2;
-
-    //四則演算結果の表示
-    std::cout << "Summation is " << num1 + num2 << std::endl << "Difference is " << num1 - num2 << std::endl << "Product is " << num1 * num2 << std::endl << "Quotient is " << num1 / num2 << std::endl;
-
+   std::cout << "Imput first sentence" << std::endl;
+   std::cin >> string1;
+   std::cout << "Imput second sentence" << std::endl;
+   std::cin >> string2;
+   std::cout << string1 << string2 <<std::endl;
     return 0;
 
 }
